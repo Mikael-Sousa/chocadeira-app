3c6f6d2300cb67c660500909eb1f5eadfe625dea
Mikael-Sousa <mikael.sr.dev@gmail.com>
2026-06-25 09:18:52 -0300

    add logs in src/app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,20 +1,29 @@
+console.log("1 - app.ts carregado");
+
 import http from "http";
 import { WebSocketServer } from "ws";
 import app from "./transports/http/app";
 import { setupWS } from "./transports/websocket";
 import { simulator } from "./transports/websocket/services/simulator";
 
+console.log("2 - imports carregados");
+
 const prompt = require("prompt-sync")();
 
-const PORT = 4000;
+const PORT = Number(process.env.PORT) || 4000;
+
+console.log("3 - criando servidor HTTP");
 
 // cria servidor HTTP
 const server = http.createServer(app);
+console.log("4 - criando websocket")
 
 // pluga WebSocket no mesmo server
 const wss = new WebSocketServer({ server });
+console.log("5 - configurando websocket");
 
 setupWS(wss);
+console.log("6 - iniciando listen");
 
 /* const res = prompt("Is it a simulation? y/n: ");
 
